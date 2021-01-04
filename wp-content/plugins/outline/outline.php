5d27c2203cd6b4b69b0cb1543ac773ff3a64b616
Your Name <you@example.com>
2021-01-04 21:13:46 +0100

    add logic for order

--- wp-content/plugins/outline/outline.php
+++ wp-content/plugins/outline/outline.php
@@ -3,68 +3,28 @@
  * Plugin Name: OUTLINE
  */
 
- add_action('admin_menu', 'test_button_menu');
+ add_action( 'woocommerce_add_to_cart', 'so_payment_complete' );
+ //add_action( 'woocommerce_payment_complete', 'so_payment_complete' );
 
- function test_button_menu(){
-   add_menu_page('Test Button Page', 'Test Button', 'manage_options', 'test-button-slug', 'test_button_admin_page');
+ function so_payment_complete( $order_id ){
+     print "we are in";
+     $order = wc_get_order( $order_id );
+     $user = $order->get_user();
+     echo "order is started";
 
+    $cURLConnection = curl_init('https://167.99.202.210:17175/ThtFk8CS89LJmq1spjRN-Q//access-keysi');
+    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+    //ignore ssl validation
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+    $apiResponse = curl_exec($cURLConnection);
+    curl_close($cURLConnection);
+
+    // $apiResponse - available data from the API request
+    $jsonArrayResponse - json_decode($apiResponse);
+    echo "response is " + $jsonArrayResponse.accessUrl;
  }
 
- function test_button_admin_page() {
-
-   // This function creates the output for the admin page.
-   // It also checks the value of the $_POST variable to see whether
-   // there has been a form submission.
-
-   // The check_admin_referer is a WordPress function that does some security
-   // checking and is recommended good practice.
-
-   // General check for user permissions.
-   if (!current_user_can('manage_options'))  {
-     wp_die( __('You do not have sufficient pilchards to access this page.')    );
-   }
-
-   // Start building the page
-
-   echo '<div class="wrap">';
-
-   echo '<h2>Test Button Demo</h2>';
-
-   // Check whether the button has been pressed AND also check the nonce
-   if (isset($_POST['test_button']) && check_admin_referer('test_button_clicked')) {
-     // the button has been pressed AND we've passed the security check
-     test_button_action();
-   }
-
-   echo '<form action="options-general.php?page=test-button-slug" method="post">';
-
-   // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
-   wp_nonce_field('test_button_clicked');
-   echo '<input type="hidden" value="true" name="test_button" />';
-   submit_button('Call Function');
-   echo '</form>';
-
-   echo '</div>';
-
- }
-
- function test_button_action()
- {
-   echo '<div id="message" class="updated fade"><p>'
-     .'The "Call Function" button was clicked.' . '</p></div>';
-
-   $path = WP_TEMP_DIR . '/test-button-log.txt';
-
-   $handle = fopen($path,"w");
-
-   if ($handle == false) {
-     echo '<p>Could not write the log file to the temporary directory: ' . $path . '</p>';
-   }
-   else {
-     echo '<p>Log of button click written to: ' . $path . '</p>';
-
-     fwrite ($handle , "Call Function button clicked on: " . date("D j M Y H:i:s", time()));
-     fclose ($handle);
-   }
- }
  ?>
